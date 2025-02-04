b54cb6fc1fc35490938b2db541a0882abd4fc2df
Sandra Mingo <sandra.mingo.ramirez1@gmail.com>
2025-02-04 20:05:39 +0100

    práctica 1 - leer datos con pandas

---
+++ Aprendizaje_Automatico/ejercicios/Notebook1/my_modules/my_module.py
@@ -0,0 +1,17 @@
+#Example of definition of a new module.
+#A module is a file containing Python definitions and statements.
+#The file name is the module name with the suffix .py appended.
+#Within a module, the module’s name (as a string) is available as
+#the value of the global variable __name__.
+
+def mapSEX(x):
+    if x == 1:
+        return "MALE"
+    else:
+        return "FEMALE"
+
+#You can define new functions
+
+##def my_function(variables):
+##    Your code
+##    return variables
